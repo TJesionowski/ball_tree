5168d201e271ec16f7daee39ac7c1ac88095766b
Timothy Jesionowski <timothy.n.jesionowski@vertaeon.com>
2022-12-04 14:54:54 -0600

    Got points on a plane with a circular bound.
    So much learning...

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from manim import *
-class AxesTemplate(Scene):
+import numpy as np
+
+# Seed chosen by unfair coinflip because I don't have dice handy.
+rng = np.random.default_rng(0)
+assert rng.bytes(8).hex() == "5f82c2d9cfeb0fa3"
+
+class CreateBallTree(Scene):
     def construct(self):
-        graph = Axes(
-            x_range=[-1,10,1],
-            y_range=[-1,10,1],
-            x_length=9,
-            y_length=6,
-            axis_config={"include_tip":False}
-        )
-        labels = graph.get_axis_labels()
-        self.add(graph, labels)
+        plane = NumberPlane()
+
+        # Generate 3d points then mask the 3rd dimension so it doesn't mess up our calculations
+        var=4
+        points3 = rng.multivariate_normal([0,0,0],
+                                          var*np.identity(3),
+                                          size=10)
+        points = points3
+        points[:,-1] = 0
+        points = points
+
+        dots = [Dot(p) for p in points]
+        self.add(plane, *dots)
+
+        centroid = points.mean(axis=0)
+        radius = max(np.linalg.norm(centroid-points, axis=1))
+        self.play(Create(Circle(radius=radius, arc_center=centroid)))
+
+        self.wait()
